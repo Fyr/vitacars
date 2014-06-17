a9a92f23c79c81dd777a044a54cac7a9dac3f1f3
belcoder <belcoderphp@gmail.com>
2014-06-17 15:17:58 +0300

    #306846 Загрузка данных из CSV

--- app/Controller/AdminUploadCsvController.php
+++ app/Controller/AdminUploadCsvController.php
@@ -22,27 +22,63 @@ class AdminUploadCsvController extends AdminController {
             $fileCsv = file($_FILES['csv_file']['tmp_name']);
             if ($fileCsv) {
                 // обработка файла
-                unset($fileCsv[0]);
                 $csv = array();
                 foreach ($fileCsv as $key => $value) {
                     $a = explode(';', $fileCsv[$key]);
-                    if (isset($csv[$a[0]])) {
-                        $csv[$a[0]] += intval($a[1]);
-                    } else {
-                        $csv[$a[0]] = intval($a[1]); 
+                    foreach ($a as $key_ => $value_) {
+                        $csv[$key][$key_] = trim($value_);
                     }
                 }
-                // обновление количества в БД
-                foreach ($csv as $key => $value) {
+
+                $keys = $csv[0];
+                unset($csv[0]);
+                
+                /* Просуммируем данные по одинаковым номерам */
+                $usedNumbers = array();
+                foreach ($csv as $number) {
+                    if (isset($usedNumbers[$number[0]])) {
+                        foreach ($number as $key => $value) {
+                            if ($key) {
+                                $usedNumbers[$number[0]][$key] = intval($value) + $usedNumbers[$number[0]][$key];
+                            }
+                        }
+                    } else {
+                        foreach ($number as $key => $value) {
+                            if ($key) {
+                                $usedNumbers[$number[0]][$key] = intval($value);
+                            }
+                        }
+                        
+                    }
+                }
+                foreach ($usedNumbers as $key => $value) {
+                    /* Обновляем поле количество */
                     $productId = $this->FormValues->find('first', array(
-                        'fields' => array('object_id'), 
+                        'fields' => array('object_id', 'id'), 
                         'conditions' => array(
                             'field_id' => 5,
                             'value LIKE' => '%'.$key.'%'
                         )
                     ));
                     if ($productId) {
-                        $this->Article->save(array('id' => $productId['FormValues']['object_id'], 'count' => $value));
+                        /* Изменим кол-во */
+                        $this->Article->save(array('id' => $productId['FormValues']['object_id'], 'count' => $usedNumbers[$key][1]));
+                    
+                        /* Изменим кол-во по данным с ключами */
+                        unset($keys[0]);
+                        unset($keys[1]);
+                        foreach ($keys as $key_ => $value_) {
+                            $field_id = $this->FormField->find('first', array(
+                                'fields' => array('id'), 
+                                'conditions' => array('key' => $value_)
+                            ));
+                            if ($field_id ) {
+                                $row = $this->FormValues->find('first', array(
+                                    'fields' => array('id'),
+                                    'conditions' => array('field_id' => $field_id['FormField']['id'], 'object_id' => $productId['FormValues']['object_id'])));
+                                $this->FormValues->save(array('id' => $row['FormValues']['id'], 'value' => $usedNumbers[$key][$key_]));
+                            }
+                        }
                     }
                 }
                 $this->Session->setFlash(__('File successfully downloaded and read'));
