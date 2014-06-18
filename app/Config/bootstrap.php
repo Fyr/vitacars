bcb4b768d7876b921cf11bb669d8f616af1f0cdc
belcoder <belcoderphp@gmail.com>
2014-06-18 17:21:16 +0300

    Task #310671 Сделать разделители для столбцов

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -27,8 +27,8 @@ define('RECAPTCHA_PRIVATE_KEY', '6Lezy-QSAAAAACCM1hh6ceRr445OYU_D_uA79UFZ');
 Configure::write('Recaptcha.publicKey', RECAPTCHA_PUBLIC_KEY);
 Configure::write('Recaptcha.privateKey', RECAPTCHA_PRIVATE_KEY);
 
-define('DOMAIN_NAME', 'vitacars.dev');
-define('DOMAIN_TITLE', 'vitacars.dev');
+define('DOMAIN_NAME', 'VitaCars.ru');
+define('DOMAIN_TITLE', 'VitaCars.ru');
 
 define('AUTH_ERROR', __('Invalid username or password, try again'));
 define('TEST_ENV', $_SERVER['SERVER_ADDR'] == '192.168.1.31');
@@ -37,6 +37,9 @@ define('EMAIL_ADMIN', 'fyr.work@gmail.com');
 define('EMAIL_ADMIN_CC', 'fyr.work@gmail.com');
 
 define('PATH_FILES_UPLOAD', $_SERVER['DOCUMENT_ROOT'].'/files/');
+
+define('SEPARATOR_DEICHARGE', '&nbsp;');
+define('SEPARATOR_DECIMAL', ',');
 
 function fdebug($data, $logFile = 'tmp.log', $lAppend = true) {
 		file_put_contents($logFile, mb_convert_encoding(print_r($data, true), 'cp1251', 'utf8'), ($lAppend) ? FILE_APPEND : null);
