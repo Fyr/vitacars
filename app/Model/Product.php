ba05eb360c490132a601e88ba6c535976ab25bad
Fyr <fyr@tut.by>
2015-12-25 14:08:43 +0300

    Правки для текст.поиска

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,6 +3,8 @@ App::uses('AppModel', 'Model');
 App::uses('Article', 'Article.Model');
 App::uses('Media', 'Media.Model');
 App::uses('Category', 'Model');
+App::uses('Subcategory', 'Model');
+App::uses('Brand', 'Model');
 App::uses('PMFormData', 'Form.Model');
 App::uses('Seo', 'Seo.Model');
 class Product extends Article {
@@ -12,6 +14,12 @@ class Product extends Article {
 	public $belongsTo = array(
 		'Category' => array(
 			'foreignKey' => 'cat_id'
+		),
+		'Subcategory' => array(
+			'foreignKey' => 'subcat_id'
+		),
+		'Brand' => array(
+			'foreignKey' => 'brand_id'
 		)
 	);
 	
@@ -33,9 +41,29 @@ class Product extends Article {
 			'foreignKey' => 'object_id',
 			'conditions' => array('Seo.object_type' => 'Product'),
 			'dependent' => true
+		),
+		'Search' => array(
+			'foreignKey' => 'id',
+			'dependent' => true
 		)
 	);
 	
 	public $objectType = 'Product';
-	
+
+	public function afterSave($created, $options = array()) {
+		$subcategory = $this->Subcategory->findById($this->data['Product']['subcat_id']);
+		$brand = $this->Brand->findById($this->data['Product']['brand_id']);
+		$this->data['Search']['id'] = $this->id;
+		$this->data['Search']['body'] = implode(',', array(
+			$this->data['Product']['code'],
+			str_replace(', ', ',', $this->data['Product']['detail_num']),
+			$this->data['Product']['title'],
+			$this->data['Product']['title_rus'],
+			$this->data['Product']['motor'],
+			$subcategory['Subcategory']['title'],
+			$subcategory['Category']['title'],
+			$brand['Brand']['title']
+		));
+		$this->Search->save($this->data['Search']);
+	}
 }
---
+++ app/Model/Search.php
@@ -0,0 +1,5 @@
+<?php
+App::uses('AppModel', 'Model');
+class Search extends AppModel {
+	public $useTable = 'search';
+}
