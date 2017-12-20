3b1f9e7974a13c66320dcf577297e19155316e6a
Fyr <fyr@tut.by>
2017-12-21 01:20:15 +0300

    Рефакторинг

--- app/Console/Command/Task/RecalcFormulaTask.php
+++ app/Console/Command/Task/RecalcFormulaTask.php
@@ -10,9 +10,7 @@ class RecalcFormulaTask extends AppShell {
         $this->Task->setProgress($this->id, 0, $total);
         $this->Task->setStatus($this->id, Task::RUN);
 
-        $fields = array('key', 'value');
-        $conditions = array('PMFormConst.object_type' => 'SubcategoryParam');
-        $aConst = $this->PMFormConst->find('list', compact('fields', 'conditions'));
+        $aConst = $this->PMFormConst->getData();
 
         $page = 1;
         $limit = 1000;
@@ -30,9 +28,7 @@ class RecalcFormulaTask extends AppShell {
                     throw new Exception(__('Processing was aborted by user'));
                 }
 
-                $xPrices = $this->FormPrice->findAllByProductId($row['PMFormData']['object_id']);
-                $aExclude = ($xPrices) ? Hash::extract($xPrices, '{n}.FormPrice.fk_id') : array();
-                $this->PMFormData->_recalcFormula($row, $fields, $aConst, $aExclude);
+                $this->PMFormData->recalcFormula($row, $fields, $aConst);
 
                 $i++;
                 $this->Task->setProgress($this->id, $i);
@@ -43,35 +39,5 @@ class RecalcFormulaTask extends AppShell {
         $this->Task->setData($this->id, 'xdata', $total);
         $this->Task->setStatus($this->id, Task::DONE);
     }
-/*
-    private function run($taskI, $total) {
-        $subtask_id = $this->Task->add(0, 'TestProgress_task'.$taskI, null, $this->id);
-        $this->Task->setData($this->id, 'subtask_id', $subtask_id);
-        $this->Task->setProgress($subtask_id, 0, $total);
-        $this->Task->setStatus($subtask_id, Task::RUN);
-        for($i = 0; $i < $total; $i++) {
-            $status = $this->Task->getStatus($this->id);
-            if ($status == Task::ABORT) {
-                $this->Task->setStatus($subtask_id, Task::ABORTED);
-                throw new Exception(__('Processing was aborted by user'));
-            }
 
-            sleep(1);
-            $this->Task->setProgress($subtask_id, $i + 1);
-
-            $_progress = $this->Task->getProgressInfo($subtask_id);
-            $progress = $this->Task->getProgressInfo($this->id);
-            $this->Task->setProgress($this->id, $progress['progress'] + $_progress['percent'] * 0.01);
-
-            if ($i > 60) {
-                throw new Exception(__('Too much iterations'));
-            }
-        }
-
-        $this->Task->setStatus($subtask_id, Task::DONE);
-
-        $this->Task->setProgress($this->id, $taskI);
-        $this->Task->saveStatus($this->id);
-    }
-*/
 }
